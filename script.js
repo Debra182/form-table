62ae9d35fc3068e938f5f9afb84a674b1251eea2
Debra182 <dotachristy@yahoo.com>
2023-08-21 17:29:42 +0300

    css

--- script.js
+++ script.js
@@ -217,5 +217,24 @@ if (result==true) {
         window.location='';
     }
 }
+/*
+function Convert() {
+    var table = document.getElementById("data");
+    var header = [];
+    var rows = [];
 
+    for (var i = 0; i < table.rows[0].cells.length; i++) {
+        header.push(table.rows[0].cells[i].innerHTML);
+    }
 
+    for (var i = 1; i < table.rows.length; i++) {
+        var row = {};
+        for (var j = 0; j < table.rows[i].cells.length; j++) {
+            row[header[j]] = table.rows[i].cells[j].innerHTML;
+        }
+        rows.push(row);
+    }
+
+    alert(JSON.stringify(rows));
+}
+*/
